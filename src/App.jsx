e0a1e7c616c556af48c80fd7700e9220fbbd6ec4
Isaac Huang <isaac_huang@gss.com.tw>
2021-05-31 11:01:47 +0800

    chore: install Semantic UI React

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { NewCategoryApp } from "./apps";
 import "jquery-ui-dist/jquery-ui.min.css";
+import 'semantic-ui-css/semantic.min.css';
 import "./scss/index.scss";
 
 export default function App() {
