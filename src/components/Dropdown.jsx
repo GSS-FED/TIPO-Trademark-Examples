865f3304ec0a8ee999334326d544d427e55c2f0e
Isaac Huang <isaac_huang@gss.com.tw>
2021-05-31 12:53:49 +0800

    feat: customize the dropdown

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import * as S from 'semantic-ui-react';
+
+export const Dropdown = styled(S.Dropdown)`
+  box-sizing: border-box;
+  height: 40px;
+  line-height: 40px;
+  background-color: #fff;
+  padding: 0 12px;
+  border: 1px solid #d8e2f5;
+  border-radius: 4px;
+
+  .divider.text {
+    width: 182px;
+    padding-right: 12px;
+    border-right: 1px solid #d8e2f5;
+  }
+
+  .menu {
+    width: 100%;
+  }
+`;
+
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,3 +3,4 @@ export * as Category from "./Category";
 export * as SubModal from "./SubModal";
 export { Modal } from "./Modal";
 export * as Tab from "./Tab";
+export { Dropdown } from "./Dropdown";
