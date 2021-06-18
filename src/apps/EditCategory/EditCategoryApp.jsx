89af6a347a0c81e4dd2ab5f63ab0b03ed9ce3e2b
Isaac Huang <isaac_huang@gss.com.tw>
2021-06-18 20:28:30 +0800

    feat: implement custom category editing

--- src/apps/EditCategory/EditCategoryApp.jsx
+++ src/apps/EditCategory/EditCategoryApp.jsx
@@ -1,6 +1,8 @@
 import { useState, useCallback, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
 import { v4 as uuidv4 } from "uuid";
 import { Modal } from "../../components";
 import * as API from "../../api";
@@ -34,11 +36,48 @@ const CategoryCreator = ({ id, className, onSubmit }) => {
   );
 };
 
-const CustomCategory = ({ id, className, category }) => {
+const CustomCategory = ({ id, className, category, onChange }) => {
+  const [isEditing, setEditing] = useState(false);
+  const [value, setValue] = useState(category.content);
+
+  const handleValueChange = useCallback((evt) => {
+    setValue(evt.target.value);
+  }, []);
+
+  const handleChange = useCallback(() => {
+    setEditing(false);
+    if (typeof onChange !== "function") return;
+    onChange({
+      ...category,
+      title: value,
+      content: value,
+    });
+  }, [onChange, category, value]);
+
+  const handleCancel = useCallback(() => {
+    setEditing(false);
+  }, []);
+
+  const handleEdit = useCallback(() => {
+    setValue(category.content);
+    setEditing(true);
+  }, [category]);
+
   return (
     <div id={id} className={className}>
       <span>[自訂商品]</span>
-      <span>{category.content}</span>
+      {isEditing ? (
+        <>
+          <input value={value} onChange={handleValueChange} />
+          <button onClick={handleChange}>確定</button>
+          <button onClick={handleCancel}>取消</button>
+        </>
+      ) : (
+        <>
+          <span>{category.content}</span>
+          <FontAwesomeIcon icon={faPen} onClick={handleEdit} />
+        </>
+      )}
     </div>
   );
 };
@@ -61,6 +100,13 @@ export const EditCategoryApp = () => {
     setCustomCategories((cs) => [...cs, cat]);
   }, []);
 
+  const handleEditCategory = useCallback(
+    (i) => (cat) => {
+      setCustomCategories((cs) => [...cs.slice(0, i), cat, ...cs.slice(i + 1)]);
+    },
+    []
+  );
+
   useEffect(() => {
     API.categories().then(setCategories);
   }, []);
@@ -68,8 +114,12 @@ export const EditCategoryApp = () => {
   return (
     <Container open clear title="編輯類別" onCancel={() => history.push("/")}>
       <CategoryCreator onSubmit={handleCreateCategory} />
-      {customCategories.map((cat) => (
-        <CustomCategory key={cat.id} category={cat} />
+      {customCategories.map((cat, i) => (
+        <CustomCategory
+          key={cat.id}
+          category={cat}
+          onChange={handleEditCategory(i)}
+        />
       ))}
       {categories.map((cat) => (
         <BuiltInCategory key={cat.id} category={cat} />
