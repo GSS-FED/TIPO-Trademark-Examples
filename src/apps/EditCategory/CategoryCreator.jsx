548de5231051c1d7da956635c66aa085bc70aec7
Isaac Huang <isaac_huang@gss.com.tw>
2021-06-21 18:05:09 +0800

    feat: style the category creator component

--- src/apps/EditCategory/CategoryCreator.jsx
+++ src/apps/EditCategory/CategoryCreator.jsx
@@ -1,8 +1,31 @@
 import { useState, useCallback } from "react";
+import styled from "styled-components";
 import { v4 as uuidv4 } from "uuid";
-import { Button } from "../../components";
+import { Button, Input } from "../../components";
 import Select from "../../Select";
 import * as List from "./List";
+
+const Container = styled(List.Title)`
+  display: flex;
+  flex-direction: row;
+  border: 1px solid #cce1ff;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+  padding-top: 15px;
+  padding-bottom: 15px;
+`;
+
+const TitleSelect = styled(Select)`
+  margin-right: 12px;
+  .form_selectInput_label {
+    display: flex;
+    flex-direction: row;
+  }
+`;
+
+const TitleInput = styled(Input)`
+  margin-right: 12px;
+`;
 
 export const CategoryCreator = ({
   id,
@@ -25,14 +48,17 @@ export const CategoryCreator = ({
   }, [onSubmit, value]);
 
   return (
-    <List.Title id={id} className={className}>
-      <Select checked={checked} onChange={onSelect}>
-        <span>自訂商品服務名稱</span>
-        <input value={value} onChange={(evt) => setValue(evt.target.value)} />
-        <Button type="record" disabled={!value} onClick={handleSubmit}>
-          新增
-        </Button>
-      </Select>
-    </List.Title>
+    <Container id={id} className={className}>
+      <TitleSelect checked={checked} onChange={onSelect}>
+        自訂商品服務名稱
+      </TitleSelect>
+      <TitleInput
+        value={value}
+        onChange={(evt) => setValue(evt.target.value)}
+      />
+      <Button type="record" disabled={!value} onClick={handleSubmit}>
+        新增
+      </Button>
+    </Container>
   );
 };
